7a6ea85cf946ec30e9ee054291c3c5d38ad2205a
VY <veasna.youphon@live.com>
2024-08-18 23:39:35 -0700

    complete daily_wages

--- part01-27_daily_wages/src/daily_wages.py
+++ part01-27_daily_wages/src/daily_wages.py
@@ -1,1 +1,9 @@
 # Write your solution here
+
+hr_wage = float(input("Hourly wage: "))
+hr_worked = int(input("Hours worked: "))
+day_of_week = input("Day of the week: ")
+if day_of_week == 'Sunday':
+    hr_wage = hr_wage*2
+    
+print(f"Daily wages: {hr_wage*hr_worked} euros")
