26238cb0219072d7a8a837f839f1efd5e31fbe30
VY <veasna.youphon@live.com>
2024-08-18 23:26:10 -0700

    complete temperatures

--- part01-26_temperatures/src/temperatures.py
+++ part01-26_temperatures/src/temperatures.py
@@ -1,1 +1,6 @@
 # Write your solution here
+temp_in_fahrenheit = float(input("Please type in a temperature (F): "))
+celsius = (temp_in_fahrenheit-32)*5/9
+print(f"{temp_in_fahrenheit} degrees Fahrenheit equals {celsius} degrees Celsius")
+if celsius < 0:
+    print("Brr! It's cold in here!")
