799a97fe1cb1438957608826f48124b0a91ec31a
VY <veasna.youphon@live.com>
2024-08-18 22:40:07 -0700

    complete students_in_groups

--- part01-20_students_in_groups/src/students_in_groups.py
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -1,1 +1,5 @@
 # Write your solution here
+import math
+number_of_student = int(input("How many students on the course? "))
+group_size = int(input("Desire group size? "))
+print(f"Number of groups formed: {math.ceil(number_of_student/group_size)}")
