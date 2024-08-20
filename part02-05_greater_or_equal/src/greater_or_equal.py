c3058b61aa8ce0dce6a1dfd39a0517ff9d50da17
VY <veasna.youphon@live.com>
2024-08-19 18:08:43 -0700

    complete greater_or_equal

--- part02-05_greater_or_equal/src/greater_or_equal.py
+++ part02-05_greater_or_equal/src/greater_or_equal.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+first_num = int(input("Please type in the first number: "))
+second_num = int(input("Please type in another number: "))
+
+if first_num > second_num:
+    print(f"The greater number was: {first_num}")
+elif first_num < second_num:
+    print(f"The greater number was: {second_num}")
+else:
+    print("The numbers are equal!")
