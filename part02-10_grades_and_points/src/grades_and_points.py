eb4060987ca64c021e773dd03096138f4ce9d22d
VY <veasna.youphon@live.com>
2024-08-19 19:24:52 -0700

    complete grades_and_points

--- part02-10_grades_and_points/src/grades_and_points.py
+++ part02-10_grades_and_points/src/grades_and_points.py
@@ -1,1 +1,20 @@
 # Write your solution here
+point = int(input("How many points [0-100]: "))
+
+if point < 0 or point > 100:
+    grade = "impossible!"
+    
+elif point >= 90:
+    grade = 5    
+elif point >= 80:
+    grade = 4
+elif point >= 70:
+    grade = 3
+elif point >= 60:
+    grade = 2
+elif point >= 50:
+    grade = 1
+else:
+    grade = "fail"
+    
+print(f"Grade: {grade}")
