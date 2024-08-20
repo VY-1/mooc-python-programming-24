a03997d26875919128e77fdeb6c9e6b2fdbb7345
VY <veasna.youphon@live.com>
2024-08-19 17:52:56 -0700

    complete typecasting module

--- part02-03_typecasting/src/typecasting.py
+++ part02-03_typecasting/src/typecasting.py
@@ -1,1 +1,8 @@
 # Write your solution here
+
+float_num = float(input("Please type in a number: "))
+num_int = int(float_num)
+num_dec = float_num-num_int
+
+print(f"Integer part: {num_int}")
+print(f"Decimal part: {num_dec}")
