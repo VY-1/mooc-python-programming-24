6eb06e670853d704fe23f3e0a18a3be9defeab87
VY <veasna.youphon@live.com>
2024-08-20 16:59:22 -0700

    repeat_password

--- part02-18_repeat_password/src/repeat_password.py
+++ part02-18_repeat_password/src/repeat_password.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+password = input("Password: ")
+
+while True:
+    repeat = input("Repeat password: ")
+    if password == repeat:
+        break
+    
+    print("They do not match!")
+    
+print("User account created!")
