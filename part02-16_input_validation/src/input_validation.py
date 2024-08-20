191879f470100357924e428169180647c427e1c2
VY <veasna.youphon@live.com>
2024-08-20 16:51:12 -0700

    complete input_validation

--- part02-16_input_validation/src/input_validation.py
+++ part02-16_input_validation/src/input_validation.py
@@ -1,2 +1,16 @@
 from math import sqrt
 # Write your solution here
+
+while True:
+    
+    number = int(input("Please type in a number: "))
+    #Checking edge cases
+    if number == 0:
+        break
+    
+    if number < 0:
+        print("Invalid number")
+    else:
+        print(f"{sqrt(number)}")
+        
+print("Exiting...")
