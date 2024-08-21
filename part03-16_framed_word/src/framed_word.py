f0f422179ab48ae4d2622bd9a87d0e32927e7a03
VY <veasna.youphon@live.com>
2024-08-21 16:19:51 -0700

    complete framed_word

--- part03-16_framed_word/src/framed_word.py
+++ part03-16_framed_word/src/framed_word.py
@@ -1,1 +1,16 @@
 # Write your solution here
+import math
+
+word = input("Word: ")
+
+#seting up padding between word
+frame_padding = (28 -len(word))//2
+frame_padding = " "*frame_padding
+
+#Display base on word length odd or even
+print("*"*30)
+if len(word) % 2 !=0:
+    print(f"*{frame_padding}{word} {frame_padding}*")
+else:
+    print(f"*{frame_padding}{word}{frame_padding}*")
+print("*"*30)
