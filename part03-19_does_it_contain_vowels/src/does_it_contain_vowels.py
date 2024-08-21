954265951c358d39d82f9b394ce6393d649004e7
VY <veasna.youphon@live.com>
2024-08-21 16:47:11 -0700

    complete does_it_contain_vowels

--- part03-19_does_it_contain_vowels/src/does_it_contain_vowels.py
+++ part03-19_does_it_contain_vowels/src/does_it_contain_vowels.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+word = input("Please type in a string: ")
+vowels ="aeo"
+counter = 0
+
+while counter < 3:
+    if vowels[counter] in word:
+        print(f"{vowels[counter]} found")
+    else:
+        print(f"{vowels[counter]} not found")
+        
+    counter+=1
