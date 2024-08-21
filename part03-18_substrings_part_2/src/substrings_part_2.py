311156a966d041db4c27581ae0e722287c6df506
VY <veasna.youphon@live.com>
2024-08-21 16:34:39 -0700

    complete substrings_part_2

--- part03-18_substrings_part_2/src/substrings_part_2.py
+++ part03-18_substrings_part_2/src/substrings_part_2.py
@@ -1,1 +1,10 @@
 # Write your solution here
+
+word = input("Please type in a string: ")
+
+word_length = len(word)
+count = len(word)-1
+
+while count >= 0:
+    print(word[count: word_length])
+    count-=1
