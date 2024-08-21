d72954e05764daff78c7bc9ff1e786913ed4547d
VY <veasna.youphon@live.com>
2024-08-20 18:06:39 -0700

    complete story

--- part02-21_story/src/story.py
+++ part02-21_story/src/story.py
@@ -1,1 +1,16 @@
 # Write your solution here
+
+all_string = ""
+last_word = ""
+
+while True:
+    word = input("Please type in a word: ")
+    
+    if word == "end" or word == last_word:
+        break
+    
+    all_string += word + " "
+    last_word = word
+    
+
+print(all_string)
