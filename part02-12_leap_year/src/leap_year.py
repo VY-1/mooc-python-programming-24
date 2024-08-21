90c4156fd1e499fd402ebd0a5e707a2ba09abfeb
VY <veasna.youphon@live.com>
2024-08-20 17:48:43 -0700

    update leap_year and next_leap_year

--- part02-12_leap_year/src/leap_year.py
+++ part02-12_leap_year/src/leap_year.py
@@ -2,7 +2,18 @@
 
 year = int(input("Please type in a year: "))
 
-if year % 4 == 0 and (year % 100 !=0 or year % 400 == 0):
+# if year % 4 == 0 and (year % 100 !=0 or year % 400 == 0):
+#     print("That year is a leap year.")
+# else:
+#     print("That year is not a leap year")
+
+# ALTERNATIVE
+if year % 100 == 0:
+    if year % 400 == 0:
+        print("That year is a leap year.")
+    else:
+        print("That year is not a leap year")
+elif year % 4 == 0:
     print("That year is a leap year.")
 else:
     print("That year is not a leap year")
--- part02-20_next_leap_year/src/next_leap_year.py
+++ part02-20_next_leap_year/src/next_leap_year.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+year = int(input("Year: "))
+next_leap_year = year
+
+while True:
+    
+    next_leap_year +=1
+
+    if next_leap_year % 4 == 0 and (next_leap_year % 100 !=0 or next_leap_year % 400 == 0):
+        print(f"The next leap year after {year} is {next_leap_year}")
+        break
