676a25fef6f25461a2bb94c9009203cc87521f32
VY <veasna.youphon@live.com>
2024-08-21 15:21:51 -0700

    complete second_and_second_to_last

--- part03-11_second_and_second_to_last/src/second_and_second_to_last.py
+++ part03-11_second_and_second_to_last/src/second_and_second_to_last.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+word = input("Please type in a string: ")
+
+second_char = word[1]
+second_to_last_char = word[len(word)-2]
+
+if second_char == second_to_last_char:
+    print(f"The second and the second to last characters are {second_char}")
+else:
+    print(f"The second and the second to last characters are different")
