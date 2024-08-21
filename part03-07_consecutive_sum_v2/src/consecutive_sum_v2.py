3f29fd35adb427be4c7cc6a495d77c73e0fd67cb
VY <veasna.youphon@live.com>
2024-08-20 19:43:23 -0700

    complete consecutive_sum

--- part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
+++ part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
@@ -1,1 +1,19 @@
 # Write your solution here
+
+#Building String
+limit = int(input("Limit: "))
+sum_string = ""
+sum = 0
+count = 1
+
+while sum < limit:
+    sum += count
+    
+    if sum < limit:
+        sum_string += f"{count} + "
+    else:
+        sum_string += f"{count} = "
+    
+    count+=1
+    
+print(f"The consecutive sum: {sum_string}{sum}")
