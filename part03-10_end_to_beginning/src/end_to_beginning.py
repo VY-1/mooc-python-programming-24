534ba37e0260caab3a58dc135f6530d2dfefde92
VY <veasna.youphon@live.com>
2024-08-21 15:07:47 -0700

    end_to_beginning

--- part03-10_end_to_beginning/src/end_to_beginning.py
+++ part03-10_end_to_beginning/src/end_to_beginning.py
@@ -1,1 +1,9 @@
 # Write your solution here
+
+word = input("Please type in a string: ")
+
+last_char_index = len(word)-1
+while last_char_index >=0:
+    print(word[last_char_index])
+    last_char_index-=1
+    
