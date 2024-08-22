bc2677e7f4b59b8f4f38767c9b3b53cca7eb8918
VY <veasna.youphon@live.com>
2024-08-22 15:11:14 -0700

    complete shape

--- part04-06_triangle/src/triangle.py
+++ part04-06_triangle/src/triangle.py
@@ -7,7 +7,7 @@ def triangle(size):
     while count <= size:
         line(count, "#")
         count+=1
-
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     triangle(5)
--- part04-07_shape/src/shape.py
+++ part04-07_shape/src/shape.py
@@ -1,4 +1,18 @@
 # Copy here code of line function from previous exercise and use it in your solution
+
+def line(count, character):
+    print(character*count)
+    
+def shape(size, character, height, char2):
+    count = 1
+    while count <= size:
+        line(count, character)
+        count+=1
+    
+    while height > 0:
+        line(size, char2)
+        height -=1
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     shape(5, "x", 2, "o")
