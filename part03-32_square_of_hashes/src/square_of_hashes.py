c2aaf05a8818da7961663ed6ffefe01603224c5b
VY <veasna.youphon@live.com>
2024-08-22 10:50:46 -0700

    complete hash_squares

--- part03-32_square_of_hashes/src/square_of_hashes.py
+++ part03-32_square_of_hashes/src/square_of_hashes.py
@@ -1,4 +1,11 @@
 # Write your solution here
+def hash_square(length:int):
+    hash = "#"
+    count = 1
+    while count <= length:
+        print(hash*length)
+        count +=1
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     hash_square(5)
