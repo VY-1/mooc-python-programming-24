e9534bc9e81f7e634b0b017b4d64ef4edd9f6ab8
VY <veasna.youphon@live.com>
2024-08-21 22:26:48 -0700

    complete flip_the_pairs

--- part03-26_flip_the_pairs/src/flip_the_pairs.py
+++ part03-26_flip_the_pairs/src/flip_the_pairs.py
@@ -1,1 +1,25 @@
 # Write your solution here
+
+number = int(input("Please type in a number: "))
+
+count = 1
+
+while count <= number:
+    if count + 1 <= number:
+        print(count + 1)
+    print(count)
+    count +=2
+   
+    
+# Alternative    
+# number = int(input("Please type in a number: "))
+ 
+# index = 1
+# while index+1 <= number:
+#     print(index+1)
+#     print(index)
+#     index += 2
+ 
+# if index <= number:
+#     print(index)
+ 
