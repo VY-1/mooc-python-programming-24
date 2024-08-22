5c82c301c4a6c318a80598764e1d07fef51d718a
VY <veasna.youphon@live.com>
2024-08-21 21:52:05 -0700

    complete first_letters_of_word

--- part03-24_first_letters_of_words/src/first_letters_of_words.py
+++ part03-24_first_letters_of_words/src/first_letters_of_words.py
@@ -1,1 +1,7 @@
 # Write your solution here
+sentence = input("Please type in a sentence: ")
+
+words = sentence.split(" ")     # split sentence by " " and store as list of words
+
+for word in words:
+    print(word[0])      #print char at index 0
