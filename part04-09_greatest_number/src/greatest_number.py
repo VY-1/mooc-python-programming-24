6d38f0e2ef57d48270396f30476706bee24eedd6
VY <veasna.youphon@live.com>
2024-08-22 15:48:29 -0700

    complete greatest_number

--- part04-09_greatest_number/src/greatest_number.py
+++ part04-09_greatest_number/src/greatest_number.py
@@ -1,4 +1,12 @@
 # Write your solution here
+def greatest_number(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 >= num3:
+        return num2
+    else:
+        return num3
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     greatest = greatest_number(5, 4, 8)
