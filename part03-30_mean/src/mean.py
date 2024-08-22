e01e86c87284812fb43da0ec348bb2be4c5054d3
VY <veasna.youphon@live.com>
2024-08-22 10:42:43 -0700

    complete mean

--- part03-30_mean/src/mean.py
+++ part03-30_mean/src/mean.py
@@ -1,4 +1,10 @@
 # Write your code here
+def mean(first, second, third):
+    result = (first + second + third)/3
+    print(result)
+    
 # Testing the function
 if __name__ == "__main__":
     mean(1, 2, 3)
+    mean(5, 3, 1)
+    mean(10, 1, 1)
