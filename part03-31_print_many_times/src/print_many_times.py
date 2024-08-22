fd8d6d8b76444d0b14863d53d76afa909d655aae
VY <veasna.youphon@live.com>
2024-08-22 10:47:13 -0700

    complete print_many_times

--- part03-31_print_many_times/src/print_many_times.py
+++ part03-31_print_many_times/src/print_many_times.py
@@ -1,5 +1,12 @@
 # Write your solution here
 
+def print_many_times(text:str, times:int):
+    count =1
+    while count <= times:
+        print(text)
+        count +=1
+    
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print_many_times("python", 5)
