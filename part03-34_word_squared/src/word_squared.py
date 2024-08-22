2e756e564003323ff14945076d04adc15b4dbd46
VY <veasna.youphon@live.com>
2024-08-22 13:01:37 -0700

    complete word_squared

--- part03-34_word_squared/src/word_squared.py
+++ part03-34_word_squared/src/word_squared.py
@@ -1,1 +1,16 @@
 # Write your solution here
+
+def squared(text:str, number:int):
+    text = text * number * number
+    counter = 0
+    start = 0
+    while counter < number:
+        print(text[start:start + number])
+        start += number
+        counter += 1
+        
+        
+if __name__ == "__main__":
+    squared("ab", 3)
+    print()
+    squared("aybabtu", 5)
