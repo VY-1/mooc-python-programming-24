00dae6e4c04f00c50b3f013b861de20c0114ff6a
VY <veasna.youphon@live.com>
2024-08-22 14:58:09 -0700

    complete triangle

--- part04-06_triangle/src/triangle.py
+++ part04-06_triangle/src/triangle.py
@@ -1,8 +1,12 @@
 # Copy here code of line function from previous exercise
-
+def line(count, character):
+    print(character*count)
 def triangle(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    count = 1
+    while count <= size:
+        line(count, "#")
+        count+=1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
