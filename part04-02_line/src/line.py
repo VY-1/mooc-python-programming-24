a561ba30fd6d56fac4e29adfe11f6e9311109800
VY <veasna.youphon@live.com>
2024-08-22 13:31:57 -0700

    complete line

--- part04-02_line/src/line.py
+++ part04-02_line/src/line.py
@@ -1,4 +1,10 @@
 # Write your solution here
+
+def line(number:int, text:str):
+    if text == "":
+        text = "*"
+    print(text[0]*number)
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     line(5, "x")
