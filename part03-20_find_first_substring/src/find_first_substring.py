69f54a46e6557bb1ca0491974a19226ddba7d905
VY <veasna.youphon@live.com>
2024-08-21 17:11:50 -0700

    complete find_first_substring

--- part03-20_find_first_substring/src/find_first_substring.py
+++ part03-20_find_first_substring/src/find_first_substring.py
@@ -1,1 +1,10 @@
 # Write your solution here
+
+word = input("Please type in a word: ")
+
+char = input("Please type in a character: ")
+
+char_index = word.find(char)    #the index where the char first found
+
+if char_index + 3 <= len(word):
+    print(word[char_index: char_index +3])  # print word with index of char_index to char_index +3
