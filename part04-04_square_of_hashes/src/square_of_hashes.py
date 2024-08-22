4ae67107222e843a5e7d9f2f0ee3e70eb9c588a6
VY <veasna.youphon@live.com>
2024-08-22 14:11:19 -0700

    complete square_of_hashes

--- part04-04_square_of_hashes/src/square_of_hashes.py
+++ part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,8 +1,13 @@
 # Copy here code of line function from previous exercise
+def line(length:int, character:str):
+    print(character*length)
 
 def square_of_hashes(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    count = 0
+    while count < size:
+        line(size, "#")
+        count +=1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
