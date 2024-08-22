3d5c26dd53b07193ea132d612cc1b623b97d46e6
VY <veasna.youphon@live.com>
2024-08-21 22:04:48 -0700

    complete factorial

--- part03-25_factorial/src/factorial.py
+++ part03-25_factorial/src/factorial.py
@@ -1,1 +1,19 @@
 # Write your solution here
+
+
+
+while True:
+    number = int(input("Please type in a number: "))
+    count = 1
+    factorial = 1
+    
+    if number <= 0:
+        break
+    
+    while count <= number:
+        factorial *= count
+        count += 1
+    
+    print(f"The factorial of the number {number} is {factorial}")
+
+print("Thanks and bye!")
