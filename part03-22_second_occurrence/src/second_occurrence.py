9755e64e47ccd1084daa387fc7a3290c127383fd
VY <veasna.youphon@live.com>
2024-08-21 21:16:00 -0700

    complete second_occurrence

--- part03-22_second_occurrence/src/second_occurrence.py
+++ part03-22_second_occurrence/src/second_occurrence.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+word = input("Please type in a string: ")
+substring = input("Please type in a substring: ")
+
+first_found_index = word.find(substring)
+
+second_found_index = word[first_found_index + len(substring):].find(substring)
+
+if first_found_index !=-1 and second_found_index !=-1:
+    print(f"The second occurrence of the substring is at index {first_found_index + second_found_index + len(substring)}.")
+else:
+    print(f"The substring does not occur twice in the string.")
