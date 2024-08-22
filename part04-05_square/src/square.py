83862f58954e50fe1bf1129fb70bab6e2682755e
VY <veasna.youphon@live.com>
2024-08-22 14:43:35 -0700

    complete square

--- part04-05_square/src/square.py
+++ part04-05_square/src/square.py
@@ -1,8 +1,13 @@
 # Copy here code of line function from previous exercise
+def line(length:int, character:str):
+    print(character*length)
 
 def square(size, character):
     # You should call function line here with proper parameters
-    line(4, "x")
+    count = 0
+    while count < size:
+        line(size, character)
+        count+=1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
