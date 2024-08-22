5c9f232e6fbd6c48db9fa90e0dbe402b5149518c
VY <veasna.youphon@live.com>
2024-08-21 20:53:46 -0700

    complete find_all_substrings

--- part03-21_find_all_substrings/src/find_all_substrings.py
+++ part03-21_find_all_substrings/src/find_all_substrings.py
@@ -1,1 +1,19 @@
 # Write your solution here
+
+word = input("Please type in a word: ")
+
+character = input("Please type in a character: ")
+
+
+while True:
+    char_index = word.find(character)   # Char at index
+    
+    if char_index ==-1 or len(word) - char_index < 3:
+        break
+    
+    print(word[char_index: char_index+3])
+        
+    char_index+=1           #increase char_index by 1
+    word = word[char_index:]
+
+        
