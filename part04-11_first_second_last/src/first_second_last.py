1885d4b792f4fb19faf77b54cb3d8413dc04ff20
VY <veasna.youphon@live.com>
2024-08-25 13:33:55 -0700

    complete first_second_last

--- part04-11_first_second_last/src/first_second_last.py
+++ part04-11_first_second_last/src/first_second_last.py
@@ -1,4 +1,15 @@
 # Write your solution here
+def first_word(word:str):
+    new_word = word.split(" ")      #Split words based on spaces and put it in a list
+    return new_word[0]              #returns word at index 0
+def second_word(word:str):
+    new_word = word.split(" ")
+    return new_word[1]
+def last_word(word:str):
+    new_word = word.split(" ")
+    return new_word[-1]             #returns word at the last index
+    
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
