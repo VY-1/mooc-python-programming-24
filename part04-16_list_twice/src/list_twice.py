2ad7bcbadd15d5e91d3e0ae9b9112bcbc8c94c6b
VY <veasna.youphon@live.com>
2024-08-25 15:29:09 -0700

    complete list_twice

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+
+items_list = []
+
+while True:
+    
+    new_item = int(input("New item: "))
+    
+    if new_item == 0:
+        break
+    
+    items_list.append(new_item)
+    print(f"The list now: {items_list}")
+    print(f"The list in order: {sorted(items_list)}")
+    
+print("Bye!")
