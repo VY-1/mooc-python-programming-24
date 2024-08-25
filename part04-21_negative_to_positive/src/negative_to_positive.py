316f122208913b7970dee446437e4f4e5c97ca2e
VY <veasna.youphon@live.com>
2024-08-25 16:07:47 -0700

    complete negative_to_positive

--- part04-21_negative_to_positive/src/negative_to_positive.py
+++ part04-21_negative_to_positive/src/negative_to_positive.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+user_input = int(input("Please type in a positive integer: "))
+
+for i in range(-user_input, user_input+1):
+    #continue with the loop if i == 0
+    if i == 0:
+        continue
+    
+    print(i)
+    
