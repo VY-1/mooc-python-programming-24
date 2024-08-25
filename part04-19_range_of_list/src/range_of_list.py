b1c6207921b09ce3527285e21c994a12ac8aff5b
VY <veasna.youphon@live.com>
2024-08-25 15:48:37 -0700

    complete range_of_list

--- part04-19_range_of_list/src/range_of_list.py
+++ part04-19_range_of_list/src/range_of_list.py
@@ -1,4 +1,10 @@
 # Write your solution here
+
+def range_of_list(my_int_list: list) -> int:
+    smallest = min(my_int_list)     #get the smallest num
+    largest = max(my_int_list)      #get the largest num
+    return largest - smallest       #return the difference
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
