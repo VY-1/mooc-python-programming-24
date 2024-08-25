40124e362fc9c7ca325662b9fc5042d1c7ed0f6e
VY <veasna.youphon@live.com>
2024-08-25 16:28:29 -0700

    complete anagrams

--- part04-23_anagrams/src/anagrams.py
+++ part04-23_anagrams/src/anagrams.py
@@ -1,1 +1,14 @@
 # Write your solution here
+
+def anagrams(word1:str, word2:str) -> bool:
+    return sorted(word1) == sorted(word2)
+
+# Test code
+
+if __name__ == "__main__":
+
+    print(anagrams("tame", "meta")) # True
+    print(anagrams("tame", "mate")) # True
+    print(anagrams("tame", "team")) # True
+    print(anagrams("tabby", "batty")) # False
+    print(anagrams("python", "java")) # False
