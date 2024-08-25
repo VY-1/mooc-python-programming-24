6e009050e76977bdab9a63f7516b76d14dcccc0c
VY <veasna.youphon@live.com>
2024-08-25 15:01:29 -0700

    update additional_and_removal

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,19 +1,19 @@
 # Write your solution here
 
 my_list = []
-user_input = ""
 count = 1
 while True:
+    print(f"The list is now {my_list}")
+
     user_input = input("a(d)d, (r)emove or e(x)it: ")
 
     if user_input == "d":
         my_list.append(count)
         count += 1
-    if user_input == "r":
+    if user_input == "r" and len(my_list)>0:
         my_list.pop()
         count -=1
     if user_input == "x":
         break
 
-    print(my_list)
 print("Bye!")
