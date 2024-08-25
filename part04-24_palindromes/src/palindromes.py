cfcc556f3f5bf2a1b69992330a880b2eb384d186
VY <veasna.youphon@live.com>
2024-08-25 16:51:28 -0700

    complete palindromes

--- part04-24_palindromes/src/palindromes.py
+++ part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,22 @@
 # Write your solution here
+def  palindromes(word:str) -> bool:
+    reverse_word = word[::-1]       #reverse string using slicing
+    return word == reverse_word     #returns true if it's equal, otherwise returns false
+    
+while True:
+    user_input = input("Please type in a palindrome: ")
+    if palindromes(user_input):
+        print(f"{user_input} is a palindrome!")
+        break
+    print("that wasn't a palindrome")
+
+
 # Note, that at this time the main program should not be written inside
 # if __name__ == "__main__":
-# block!
+
+#     user_input = input("Please type in a palindrome: ")
+#     if palindromes(user_input):
+#         print(f"{user_input} is a palindrome!")
+#     else:
+#         print("that wasn't a palindrome")
+        
