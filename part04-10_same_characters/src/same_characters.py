610fd4e15856882dcec6aaed48680f7c7ba4740a
VY <veasna.youphon@live.com>
2024-08-25 13:24:58 -0700

    complete same_characters

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,14 @@
 # Write your solution here
+def same_chars(word:str, num1:int, num2:int):
+    if (num1 >= len(word) or (num2 >= len(word))):
+        return False
+    if word[num1] == word[num2]:
+        return True
+    else:
+        return False
+    
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("coder", 1, 2))
+    print(same_chars("coder",1, 10))
