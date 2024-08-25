516cad5f551b9a250801633c511524ff466ed047
VY <veasna.youphon@live.com>
2024-08-25 13:58:10 -0700

    complete add_items_to_list

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,14 @@
 # Write your solution here
+
+user_input = int(input("How many items: "))
+my_items = []
+count = 1
+while count <= user_input:
+    
+    item = int(input(f"Item {count}: "))
+    my_items.append(item)
+    count+=1
+    
+print(my_items)
+    
+    
