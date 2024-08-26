34d7b8f0fcd6114626e63a1e778ccc2be4f23ce7
VY <veasna.youphon@live.com>
2024-08-26 16:05:14 -0700

    complete most_common_character

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def most_common_character(string:str):
+    most_common = string[0]
+    for ch in string:
+        if string.count(ch) > string.count(most_common):
+            most_common = ch
+    return most_common
+
+#Test Code
+if __name__ == "__main__":
+    
+    first_string = "abcdbde"
+    print(most_common_character(first_string))
+
+    second_string = "exemplaryelementary"
+    print(most_common_character(second_string))
