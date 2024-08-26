47de5c6ce6e61c1d5efd32ab2b8d99144e95fa39
VY <veasna.youphon@live.com>
2024-08-26 16:16:50 -0700

    complete no_vowels_allowed

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def no_vowels(my_string:str) ->str:
+    vowels ="aeiou"
+    new_string = ""
+    
+    for char in my_string:
+        if char not in vowels:
+            new_string +=char
+    return new_string
+
+# Test
+if __name__ == "__main__":
+    
+    my_string = "this is an example"
+    print(no_vowels(my_string))
