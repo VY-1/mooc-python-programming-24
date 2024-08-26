4eef844c4bc0e0d2574ec9fb31c0917f59906412
VY <veasna.youphon@live.com>
2024-08-26 14:17:53 -0700

    complete sum_of_positives

--- part04-25_sum_of_positives/src/sum_of_positives.py
+++ part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,15 @@
 # Write your solution here
+
+def sum_of_positives(my_list:list) -> int:
+    sum = 0
+    for num in my_list:
+        if num >0:
+            sum+=num
+    return sum
+
+
+#Test Code
+if __name__ == "__main__":
+    my_list = [1, -2, 3, -4, 5]
+    result = sum_of_positives(my_list)
+    print("The result is", result)
