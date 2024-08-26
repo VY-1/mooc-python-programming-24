ab38590cba610fa46ec348683fbea15b81f85b70
VY <veasna.youphon@live.com>
2024-08-26 15:16:29 -0700

    complete all_lingest_in_list

--- part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,36 @@
 # Write your solution here
+#Alternative
+# def all_the_longest(string_list:list) -> list:
+#     longest = 0
+#     longest_list = []
+#     for word in string_list:
+#         if len(word) > longest:
+#             longest = len(word)
+            
+#     for word in string_list:
+#         if len(word) == longest:
+#             longest_list.append(word)
+#     return longest_list
+
+def all_the_longest(names: list):
+    result = []
+ 
+    for name in names:
+        if result == [] or len(name) > len(result[0]):
+            result = [name]
+        elif len(name) == len(result[0]):
+            result.append(name)
+ 
+    return result
+
+# Test code
+if __name__ == "__main__":
+    
+    my_list = ["first", "second", "fourth", "eleventh"]
+
+    result = all_the_longest(my_list)
+    print(result) # ['eleventh']
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+    result = all_the_longest(my_list)
+    print(result) # ['dorothy', 'richard']
