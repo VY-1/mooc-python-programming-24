e90e315d93bf9101c3e6c80d50e9c9d9f6a878e2
VY <veasna.youphon@live.com>
2024-08-26 14:42:41 -0700

    complete distinct_numbers

--- part04-28_distinct_numbers/src/distinct_numbers.py
+++ part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def distinct_numbers(my_list:list) -> list:
+    new_list = []
+    for num in my_list:
+        if num not in new_list:         # add number if they are not in new_list
+            new_list.append(num)
+    
+    return sorted(new_list)
+
+# Test code
+if __name__ == "__main__":
+    
+    my_list = [3, 2, 2, 1, 3, 3, 1]
+    print(distinct_numbers(my_list)) # [1, 2, 3]
