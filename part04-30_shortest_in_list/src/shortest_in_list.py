811cd8c041992f82032ba02cfb2d9bf05e98683c
VY <veasna.youphon@live.com>
2024-08-26 14:57:26 -0700

    complete shortest_in_list

--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,20 @@
 # Write your solution here
+
+def shortest(string_list:list) -> str:
+    shortest = string_list[0]
+    for word in string_list:
+        if len(word) < len(shortest):
+            shortest = word
+    return shortest
+
+# Test Code
+if __name__ == "__main__":
+    
+    my_list = ["first", "second", "fourth", "eleventh"]
+
+    result = shortest(my_list)
+    print(result)
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+    result = shortest(my_list)
+    print(result)
