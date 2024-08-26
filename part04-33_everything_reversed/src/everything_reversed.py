aa579283e519d3a3c0a268a12b525db6b7b258ac
VY <veasna.youphon@live.com>
2024-08-26 15:43:42 -0700

    complete everything_reversed

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def everything_reversed(my_list:list) ->list:
+    reverse_list = my_list[::-1]
+    everything_reversed = []
+    for item in reverse_list:
+        everything_reversed.append(item[::-1])
+        
+    return everything_reversed
+    
+# Test Code
+
+if __name__ == "__main__":
+    
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
