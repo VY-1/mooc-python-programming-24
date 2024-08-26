eec1718c1c9c47a18164659a1bf680a6ed933fef
VY <veasna.youphon@live.com>
2024-08-26 15:28:57 -0700

    complete integers_to_strings

--- part04-32_integers_to_strings/src/integers_to_strings.py
+++ part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def formatted(my_float_list:list) ->list:
+    new_list = []
+    for num in my_float_list:
+        new_list.append(f"{num:.2f}")
+    return new_list
+
+#Test code
+if __name__ == "__main__":
+        
+    my_list = [1.234, 0.3333, 0.11111, 3.446]
+    new_list = formatted(my_list)
+    print(new_list)
