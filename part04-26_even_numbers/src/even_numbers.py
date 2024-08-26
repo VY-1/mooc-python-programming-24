b4b98820bd8c34810d33e6133941adb7eb593040
VY <veasna.youphon@live.com>
2024-08-26 14:23:54 -0700

    complete even_numbers

--- part04-26_even_numbers/src/even_numbers.py
+++ part04-26_even_numbers/src/even_numbers.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+def even_numbers(my_list:list) -> list:
+    new_list = []
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(num)
+    return new_list
+
+
+# Test Code
+if __name__ == "__main__":
+
+    my_list = [1, 2, 3, 4, 5]
+    new_list = even_numbers(my_list)
+    print("original", my_list)
+    print("new", new_list)
