b94d4f64d4c8424b6f45862acb18aed876b4dde7
VY <veasna.youphon@live.com>
2024-08-26 14:52:55 -0700

    complete length_of_longest

--- part04-29_length_of_longest/src/length_of_longest.py
+++ part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def length_of_longest(string_list: list) ->int:
+    longest = 0
+    for word in string_list:
+        if len(word) > longest:
+            longest = len(word)
+    return longest
+
+#Test code
+
+if __name__ == "__main__":
+
+    my_list = ["first", "second", "fourth", "eleventh"]
+
+    result = length_of_longest(my_list)
+    print(result)
+    
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+    result = length_of_longest(my_list)
+    print(result)
