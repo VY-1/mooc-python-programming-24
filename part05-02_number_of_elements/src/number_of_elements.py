5b6d7702d5d00192d388b3781a139ac9c5667002
VY <veasna.youphon@live.com>
2024-08-29 21:29:00 -0700

    complete number_of_elements

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def count_matching_elements(my_matrix: list, element: int) -> int:
+    match = 0
+    for row in my_matrix:
+        for item in row:
+            if item == element:
+                match +=1
+    return match
+
+if __name__ == "__main__":
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
