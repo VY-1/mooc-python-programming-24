2b216628d0903e2e25a8f02e189ab7ea10096007
VY <veasna.youphon@live.com>
2024-08-29 19:22:58 -0700

    complete longest_string

--- part05-01_longest_string/src/longest_string.py
+++ part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def longest(strings: list):
+    longest = strings[0]
+    for string in strings:
+        if len(string) > len(longest):
+            longest = string
+    return longest
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
