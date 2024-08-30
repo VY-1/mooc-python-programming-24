8cd96c76811e0a91e68542bea2c1964b4ec0d743
VY <veasna.youphon@live.com>
2024-08-29 21:34:38 -0700

    update number_of_elements

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,11 +1,11 @@
 # Write your solution here
 def count_matching_elements(my_matrix: list, element: int) -> int:
-    match = 0
+    match_count = 0
     for row in my_matrix:
         for item in row:
             if item == element:
-                match +=1
-    return match
+                match_count +=1
+    return match_count
 
 if __name__ == "__main__":
     m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
