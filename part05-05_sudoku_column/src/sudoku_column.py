b7831e294808ec155986b4b67eb4ab613eee14eb
VY <veasna.youphon@live.com>
2024-08-30 17:56:17 -0700

    complete sudoku_column

--- part05-05_sudoku_column/src/sudoku_column.py
+++ part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,24 @@
 # Write your solution here
+def column_correct(sudoku: list, column_no: int) -> bool:
+    exist_item = []
+    for row in sudoku:
+        if row[column_no] > 0 and row[column_no] in exist_item:
+            return False
+        exist_item.append(row[column_no])
+    return True
+
+if __name__ == "__main__":
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [2, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(column_correct(sudoku, 0))
+    print(column_correct(sudoku, 1))
