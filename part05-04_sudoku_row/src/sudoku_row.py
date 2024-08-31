b7451e85733a3acf8dc85d86f20eec874c4578dc
VY <veasna.youphon@live.com>
2024-08-30 17:50:08 -0700

    complete sudoku_row

--- part05-04_sudoku_row/src/sudoku_row.py
+++ part05-04_sudoku_row/src/sudoku_row.py
@@ -1,1 +1,25 @@
 # Write your solution here
+def row_correct(sudoku: list, row_no:int) -> bool:
+    exist_item = []
+    for item in sudoku[row_no]:
+        if item > 0 and item in exist_item:
+            return False
+        exist_item.append(item)
+
+    return True
+
+if __name__ == "__main__":
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [2, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(row_correct(sudoku, 0))
+    print(row_correct(sudoku, 1))
