a54dd6e4be387ce3123f764b5e4519c0152d0a19
VY <veasna.youphon@live.com>
2024-09-01 16:06:57 -0700

    complete transpose_matrix

--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,20 @@
 # Write your solution here
+
+def transpose(matrix: list):
+    new_matrix = []      #copy matirx to new_matrix, deep copy
+    for row in matrix:
+        new_matrix.append(row[:])   #copy elements in the row
+    
+    for row in range(3):
+        for col in range(3):
+            matrix[col][row] = new_matrix[row][col]    
+    
+    
+
+if __name__ == "__main__":
+    
+    matrix = [[1,2,3], [4,5,6], [7,8,9]]
+    
+    transpose(matrix)
+    
+    print(matrix)
