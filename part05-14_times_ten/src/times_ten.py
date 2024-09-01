d1f82f24eb317a267c1a200451bca04f9834bf95
VY <veasna.youphon@live.com>
2024-09-01 16:17:46 -0700

    complete times_ten

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def times_ten(start_index:int, end_index:int) ->dict:
+    new_dict = {}
+    for key in range(start_index, end_index+1):
+        new_dict[key] = key*10
+
+    return new_dict
+
+if __name__ == "__main__":
+    d = times_ten(3, 6)
+    print(d)
