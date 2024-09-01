3343a137da411473576002bd2220dcb119a3c779
VY <veasna.youphon@live.com>
2024-09-01 16:29:15 -0700

    complete factorials

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def factorials(n: int) -> dict:
+    new_dict = {}
+    result = 1
+    for value in range(1, n+1):
+        result *=value
+        new_dict[value] = result
+
+    return new_dict
+
+if __name__ == "__main__":
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
