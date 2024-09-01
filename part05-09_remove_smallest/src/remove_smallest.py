bda7184b552f3c6a8488e9f2c04a689a247b0490
VY <veasna.youphon@live.com>
2024-09-01 14:46:03 -0700

    complete remove_smallest

--- part05-09_remove_smallest/src/remove_smallest.py
+++ part05-09_remove_smallest/src/remove_smallest.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def remove_smallest(numbers:list):
+    smallest = numbers[0]       #assign the first index as smallest
+    for item in numbers:
+        if item < smallest:     #if anyitem on the list smaller than first index, set it as smallest
+            smallest = item
+
+    numbers.remove(smallest)    #remove the smallest value from list
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 1, 3, 5]
+    remove_smallest(numbers)
+    print(numbers)
