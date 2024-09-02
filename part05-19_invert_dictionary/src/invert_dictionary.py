20b96e1a75ee5de5401d6185731e52b7e3cf6dcc
VY <veasna.youphon@live.com>
2024-09-01 23:49:20 -0700

    complete invert_dictionary

--- part05-19_invert_dictionary/src/invert_dictionary.py
+++ part05-19_invert_dictionary/src/invert_dictionary.py
@@ -1,1 +1,26 @@
 # Write your solution here
+def invert(dictionary: dict):
+    new_dict = {}
+
+    for key,value in dictionary.items():
+
+        new_dict[value] = key
+        
+    dictionary.clear()
+    for key,value in new_dict.items():
+        dictionary[key] = value
+
+# Alternative
+# def invert(dictionary: dict):
+# 	copy = {}
+# 	for key in dictionary:
+# 		copy[key] = dictionary[key]
+# 	for key in copy:
+# 		del dictionary[key]
+# 	for key in copy:
+# 		dictionary[copy[key]] = key
+
+if __name__ == "__main__":
+    s = {1: "first", 2: "second", 3: "third", 4: "fourth"}
+    invert(s)
+    print(s)
