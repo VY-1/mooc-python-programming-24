759db601e9ad20147d6a249da5cf49803def2239
VY <veasna.youphon@live.com>
2024-09-01 22:34:05 -0700

    complete histogram

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def histogram(word:str):
+    new_dict = {}
+    for char in word:
+        if char not in new_dict:
+            new_dict[char] = 0
+        new_dict[char] +=1
+
+    for key, value in new_dict.items():
+        print(f"{key} {'*'*value}")
+
+
+if __name__ == "__main__":
+    histogram("statistically")
