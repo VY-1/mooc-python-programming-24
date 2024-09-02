a68a1dd773236dce069e6ad155535e5e4af2e539
VY <veasna.youphon@live.com>
2024-09-01 23:05:17 -0700

    complete phone_book_v1

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,20 @@
 # Write your solution here
+phone_book = {}
+
+while True:
+    user_input = int(input("command (1 search, 2 add, 3 quit):"))
+
+    if user_input == 3:
+        break
+    if user_input == 1:
+        name = input("name: ")
+        if name in phone_book:
+            print(phone_book[name])
+        else:
+            print("no number")
+    if user_input == 2:
+        name = input("name: ")
+        number = input("number: ")
+        phone_book[name] = number
+        print("ok!")
+print("quitting...")
