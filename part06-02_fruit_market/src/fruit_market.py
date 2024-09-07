c82e6a8951a895f51a59731c901ee0c46e68eca5
VY <veasna.youphon@live.com>
2024-09-07 15:52:30 -0700

    complete fruit_market

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,12 @@
 # write your solution here
+def read_fruits()->dict:
+    new_dict = {}
+    with open("src/fruits.csv") as new_file:
+        for line in new_file:
+            parts = line.split(";")     #split at ";" and store in parts
+            key, value = parts[0], float(parts[1])      #convert parts[1] as float and store in value
+            new_dict[key] = value       #add key, value to new_dict
+    return new_dict
+
+if __name__ == "__main__":
+    print(read_fruits())
