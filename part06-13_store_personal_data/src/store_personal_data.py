7af9323552c36af176b11dc57702615d99fa8ccf
VY <veasna.youphon@live.com>
2024-09-17 11:28:45 -0700

    complete store_personal_data

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def write_file(file_out, person:tuple):
+    with open(file_out, "a") as new_file:
+        new_file.write(f"{person[0]};{int(person[1])};{float(person[2])}\n")
+
+def store_personal_data(person:tuple):
+    write_file("people.csv", person)
+
+if __name__ == "__main__":
+    store_personal_data(("Paul Paulson", 37, 175.5))
+    store_personal_data(("Dave Paulson", 24, 139.3))
