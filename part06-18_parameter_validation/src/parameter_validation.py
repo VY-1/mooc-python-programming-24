1b0f9ba08b5205d5d3d576909c47d472fbc24e78
VY <veasna.youphon@live.com>
2024-09-23 05:47:21 -0700

    complete parameter_validation

--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def new_person(name: str, age: int):
+    person = tuple()
+
+    if name == "" or len(name.split(" ")) < 2 or len(name) > 40 or age< 0 or age > 150:
+        raise ValueError("Invalid parameters")
+
+    person = (name, age)
+    return person
+
+if __name__ == "__main__":
+
+    person1 = new_person("Don", 32)
+    person2 = new_person("Don Julio", 33)
