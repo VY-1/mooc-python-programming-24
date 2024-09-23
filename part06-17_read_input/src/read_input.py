f511871054ae584598ae51802211fff414972fa4
VY <veasna.youphon@live.com>
2024-09-23 05:26:42 -0700

    complete read_input

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,20 @@
 # Write your solution here
+def read_input(prompt:str, low:int, high:int):
+    while True:
+        user_input = input(prompt)
+
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            user_input = low-1
+
+        if user_input >= low and user_input <=high:
+            return user_input
+
+        else:
+            print(f"You must type in an integer between {low} and {high}")
+
+if __name__ == "__main__":
+
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
