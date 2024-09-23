b01a8a9a1d419e7a0a4f4e877f6a82406215d907
VY <veasna.youphon@live.com>
2024-09-23 11:21:02 -0700

    complete lottery_numbers

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,12 @@
 # Write your solution here
+from random import sample
+def lottery_numbers(amount: int, lower: int, upper: int):
+    number_list = list(range(lower, upper))
+    pick_amount = sample(number_list, amount)
+    pick_amount.sort()
+    return pick_amount
+
+if __name__ == "__main__":
+    for number in lottery_numbers(7, 1, 40):
+        print(number)
+
