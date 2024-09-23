a056db4aa11fbf0ed49abefac680236c14f5a655
VY <veasna.youphon@live.com>
2024-09-23 11:04:22 -0700

    complete calculation_with_fractions

--- part07-03_fractions/src/calculation_with_fractions.py
+++ part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,21 @@
 # Write your solution here
+import fractions
+from fractions import Fraction
+
+
+def fractionate(amount: int):
+    fraction_list =[]
+    for _ in range(amount):
+        fraction_list.append(Fraction(1, amount))
+    return fraction_list
+
+if __name__ == "__main__":
+
+    print(dir(fractions.Fraction))
+
+    for p in fractionate(3):
+        print(p)
+
+    print()
+
+    print(fractionate(5))
