616cb0b4763718525655e586e6c23ce847e6eed7
VY <veasna.youphon@live.com>
2024-09-25 09:06:29 -0700

    complete password_generator_part_1

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,12 @@
 # Write your solution here
+from random import choice
+def generate_password(lenght: int):
+    characters = "abcdefghijklmnopqrstuvwxyz"
+    password =""
+    for _ in range(lenght):
+        password +=choice(characters)
+    return password
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(generate_password(8))
