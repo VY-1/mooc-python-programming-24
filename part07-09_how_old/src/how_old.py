0357b357bef0b3b112a8a281899cbfdc53aaf3af
VY <veasna.youphon@live.com>
2024-09-26 16:31:30 -0700

    complete how_old

--- part07-09_how_old/src/how_old.py
+++ part07-09_how_old/src/how_old.py
@@ -1,1 +1,15 @@
 # Write your solution here
+from datetime import datetime
+
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
+
+birthday = datetime(year, month, day)
+millennium = datetime(1999, 12, 31)
+difference = millennium - birthday
+
+if difference.days > 0:
+    print(f"You were {difference.days} days old on the eve of the new millennium.")
+else:
+    print(f"You weren't born yet on the eve of the new millennium.")
