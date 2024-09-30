d833e0f629d4cb0a7e9954ace3e4eb3070309853
VY <veasna.youphon@live.com>
2024-09-29 18:13:42 -0700

    complete spellchecker_2

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,33 @@
-# Write your solution here
+# write your solution here
+import difflib
+from difflib import get_close_matches
+
+user_input = input("Write text: ")
+words = user_input.split(" ")
+
+
+def read_file(file_input):
+    with open(file_input) as dictionary_file:
+        word_list = []
+        suggested_word_list = {}
+        for word in dictionary_file:
+            word = word.replace("\n", "")
+            word_list.append(word)
+
+        output = ""
+        for word in words:
+            if word.lower() in word_list:
+                output +=word
+            else:
+                output += "*"+word+"*"
+            output += " "
+
+            if word.lower() not in word_list:
+                suggested_word_list[word.lower()] = get_close_matches(word.lower(), word_list)
+
+        print(output)
+        print("suggestions:")
+        for key, word in suggested_word_list.items():
+            print(f"{key}: {", ".join(word)}")
+
+read_file("src/wordlist.txt")
