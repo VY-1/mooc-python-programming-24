1504d1455c7f8de245fb4b5a56a83851331cf297
VY <veasna.youphon@live.com>
2024-10-05 19:04:29 -0700

    complete service_charge

--- part09-12_service_charge/src/service_charge.py
+++ part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,31 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self, name: str, account: str, balance: float):
+        self.__name = name
+        self.__account = account
+        self.__balance = balance
+
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__balance += amount
+            self.__service_charge()
+
+    def withdraw(self, amount: float):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            self.__service_charge()
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    def __service_charge(self):
+        self.__balance -= self.__balance * .01
 
 
+if __name__ == "__main__":
+    account = BankAccount("Randy Riches", "12345-6789", 1000)
+    account.withdraw(100)
+    print(account.balance)
+    account.deposit(100)
+    print(account.balance)
