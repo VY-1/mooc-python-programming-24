204a8f9db739a0d7e16006211a43a6108228c502
VY <veasna.youphon@live.com>
2024-10-09 11:14:35 -0700

    complete phone_book_v1

--- part10-10_phone_book_v1/src/phone_book_v1.py
+++ part10-10_phone_book_v1/src/phone_book_v1.py
@@ -53,6 +53,7 @@ class PhoneBookApplication:
         print("0 exit")
         print("1 add entry")
         print("2 search")
+        print("3 search by number")
 
     def add_entry(self):
         name = input("name: ")
@@ -67,6 +68,15 @@ class PhoneBookApplication:
             return
         for number in numbers:
             print(number)
+
+    def search_by_number(self):
+        number = input("number: ")
+        for name, numbers in self.__phonebook.all_entries().items():
+            if number in numbers:
+                print(name)
+                return
+        print("unknown number")
+        return
 
     def exit(self):
         self.__filehandler.save_file(self.__phonebook.all_entries())
@@ -84,9 +94,15 @@ class PhoneBookApplication:
                 self.add_entry()
             elif command == "2":
                 self.search()
+            elif command == "3":
+                self.search_by_number()
             else:
                 self.help()
 
-# when you run the tests, nothing apart from these two lines should be placed in the main function, outside any class definitions 
+# when you run the tests, nothing apart from these two lines should be placed in the main function, outside any class definitions
 application = PhoneBookApplication()
 application.execute()
+
+
+if __name__ == "__main__":
+    pass
