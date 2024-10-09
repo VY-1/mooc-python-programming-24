0b2edb5a20ac3e37b631656edf956511e9358735
VY <veasna.youphon@live.com>
2024-10-09 10:37:45 -0700

    complete iterable_shopping_list

--- part10-09_iterable_shopping_list/src/iterable_shopping_list.py
+++ part10-09_iterable_shopping_list/src/iterable_shopping_list.py
@@ -1,4 +1,5 @@
 # TEE RATKAISUSI TÄHÄN:
+
 class ShoppingList:
     def __init__(self):
         self.products = []
@@ -14,3 +15,26 @@ class ShoppingList:
 
     def number(self, n: int):
         return self.products[n - 1][1]
+
+    def __iter__(self):
+        self.n = 0
+
+        return self     #returns reference object as iterator is implemented with the same class definition
+
+    def __next__(self):
+        if self.n < len(self.products):
+            product = self.products[self.n]  # product at index n
+            self.n += 1  # increment index by 1
+            return product  # return product
+        else:
+            raise StopIteration  # Raise StopIterration when all products have been traversed
+
+
+if __name__ == "__main__":
+    shopping_list = ShoppingList()
+    shopping_list.add("bananas", 10)
+    shopping_list.add("apples", 5)
+    shopping_list.add("pineapple", 1)
+
+    for product in shopping_list:
+        print(f"{product[0]}: {product[1]} units")
