861b00447aa4ef5d55b52fe314c6b050005d44f2
VY <veasna.youphon@live.com>
2024-10-18 16:38:16 -0700

    complete prime_numbers

--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,20 @@
 # Write your solution here
+def prime_numbers():
+    number = 1
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+ 
+def is_prime(number: int):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+if __name__=="__main__":
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
