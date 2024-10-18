b10031833b1efbf6ef43abb8063d83b1305bb100
VY <veasna.youphon@live.com>
2024-10-18 13:38:01 -0700

    complete seasons

--- part12-02_seasons/src/seasons.py
+++ part12-02_seasons/src/seasons.py
@@ -1,1 +1,14 @@
 # Write your solution here:
+def sort_season(item: dict):
+    return item["seasons"]
+
+def sort_by_seasons(items: list):
+
+    return sorted(items, key=sort_season)
+
+if __name__=="__main__":
+    shows = [{"name": "Dexter", "rating": 8.6, "seasons": 9}, {"name": "Friends", "rating": 8.9, "seasons": 10},
+             {"name": "Simpsons", "rating": 8.7, "seasons": 32}]
+
+    for show in sort_by_seasons(shows):
+        print(f"{show['name']} {show['seasons']} seasons")
