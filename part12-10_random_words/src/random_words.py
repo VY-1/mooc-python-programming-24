59289d9ee3f21836ffa7773856b6acdd452d1383
VY <veasna.youphon@live.com>
2024-10-18 20:08:11 -0700

    complete random_words

--- part12-10_random_words/src/random_words.py
+++ part12-10_random_words/src/random_words.py
@@ -1,1 +1,21 @@
 # Write your solution here:
+from random import choice
+
+
+def word_generator(characters: str, length: int, amount: int):
+    char_string = ""
+    for i in range(amount):
+
+        for j in range(length):
+            char_string += choice(characters)
+        yield char_string
+        char_string = ""
+
+# def word_generator(letters: str, length: int, amount:int):
+#     return ("".join([choice(letters ) for i in range(length)]) for j in range(amount))
+ 
+
+if __name__=="__main__":
+    wordgen = word_generator("abcdefg", 2, 5)
+    for word in wordgen:
+        print(word)
