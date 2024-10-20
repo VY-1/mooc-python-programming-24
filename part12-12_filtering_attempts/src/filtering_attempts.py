57b0860b9c346609438df7087b95673bbab372d9
VY <veasna.youphon@live.com>
2024-10-20 16:37:58 -0700

    complete filtering_attempts

--- part12-12_filtering_attempts/src/filtering_attempts.py
+++ part12-12_filtering_attempts/src/filtering_attempts.py
@@ -8,3 +8,42 @@ class CourseAttempt:
         return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
 
 
+def accepted(attempts: list):
+    return list(filter(lambda attempt: attempt.grade >=1, attempts))
+
+def attempts_with_grade(attempts: list, grade: int):
+    return list(filter(lambda attempt: attempt.grade == grade, attempts))
+
+def passed_students(attempts: list, course: str):
+    def sort_by_name(course_attempt: CourseAttempt):
+        return course_attempt.student_name
+
+    sorted_by_name_attempts = sorted(attempts, key=sort_by_name)
+
+    return list(map(lambda name_attempt: name_attempt.student_name, filter(lambda sorted_attempt: sorted_attempt.grade > 0 and sorted_attempt.course_name == course, sorted_by_name_attempts)))
+if __name__=="__main__":
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 0)
+
+    for attempt in accepted([s1, s2, s3]):
+        print(attempt)
+
+    print()
+
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Introduction to AI", 3)
+    s4 = CourseAttempt("Olivia C. Objective", "Data Structures and Algorithms", 3)
+
+    for attempt in attempts_with_grade([s1, s2, s3, s4], 3):
+        print(attempt)
+    print()
+    print("Students who passed the course")
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to AI", 5)
+    s3 = CourseAttempt("Peter Python", "Introduction to AI", 0)
+    s4 = CourseAttempt("Jack Java", "Introduction to AI", 3)
+
+    for attempt in passed_students([s1, s2, s3, s4], "Introduction to AI"):
+        print(attempt)
